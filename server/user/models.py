5fa22e00f1db5f3de3c52644223aaf93808ceb27
Khant Thit Oo <khantthitoo.dev@gmail.com>
2025-06-03 18:06:52 +0630

    Changed Auto Increment ID To UUID

--- server/user/models.py
+++ server/user/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     full_name = models.CharField(max_length=250, blank=True, null=True)
     ph_number = models.CharField(max_length=12, blank=True, null=True)
     joined_at = models.DateTimeField(auto_now=True)
