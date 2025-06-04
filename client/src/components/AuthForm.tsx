da274ef59620a26cca7cce11cc504ee7444a0793
Khant Thit Oo <khantthitoo.dev@gmail.com>
2025-06-04 20:14:34 +0630

    frontend/auth-completed

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { baseURL } from "../../utils/axios";
 
 interface AuthFormProps {
     action: string;
 }
 
 const AuthForm: React.FC<AuthFormProps> = ({ action }) => {
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        setLoading(true);
+        try {
+            const { data } = await axios.post(`${baseURL}/auth/jwt/create`, {
+                username: username,
+                password: password,
+            });
+
+            localStorage.setItem('accessToken', data.access);
+            localStorage.setItem('refreshToken', data.refresh);
+        } catch (err: any) {
+            console.error(err)
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <form className="w-full flex flex-col gap-9 mt-10">
+        <form
+            className="w-full flex flex-col gap-9 mt-10"
+            onSubmit={handleLogin}
+        >
             {/* Username Input */}
             <div className="w-4/5 mx-auto relative">
                 <span className="absolute -translate-y-3.5 px-2 ml-4 z-10 bg-[var(--secondary)] text-lg font-medium text-[var(--primary)]">
@@ -15,6 +43,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ action }) => {
                 <input
                     type="text"
                     className="w-full h-12 border-2 rounded-lg outline-none border-[var(--primary)] text-[var(--primary)] px-2 bg-[var(--secondary)]"
+                    onChange={(e) => setUsername(e.target.value)}
                 />
             </div>
             {/* End Username Input */}
@@ -27,6 +56,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ action }) => {
                 <input
                     type="password"
                     className="w-full h-12 border-2 rounded-lg outline-none border-[var(--primary)] text-[var(--primary)] px-2 bg-[var(--secondary)]"
+                    onChange={(e) => setPassword(e.target.value)}
                 />
             </div>
             {/* End Password Input */}
